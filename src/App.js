40b7b7342ef40c0c6b98c5b337da5b7d73aee009
a-sumo <armandsumo@gmail.com>
2023-06-30 23:12:52 +0200

    always display guide

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import './App.css';
 
 function App() {
   const dialogueData = require('./components/data.json');
-  const [showGuide, setShowGuide] = useState(false);
+  const [showGuide, setShowGuide] = useState(true);
 
   return (
     <div className="App">
       <div className="app-container">
+      <Guide showGuide={showGuide} />
         <div className="conversation-container">
           <Conversation dialogue={dialogueData.dialogue} />
         </div>
-        <div className="guide-toggle-container">
+        {/* <div className="guide-toggle-container">
           <button className={`guide-toggle ${showGuide ? 'active' : ''}`} onClick={() => setShowGuide(!showGuide)}>
             {showGuide ? 'Hide Guide' : 'Show Guide'}
           </button>
-          <Guide showGuide={showGuide} />
-        </div>
+        </div> */}
       </div>
     </div>
   );
