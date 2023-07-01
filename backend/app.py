01a901eb1d8457a55581452f8fcfeea4dc69b12e
a-sumo <armandsumo@gmail.com>
2023-07-01 18:59:53 +0200

    add psycopg2 to deps

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,47 @@
+import psycopg2
 from flask import Flask, request, jsonify
-import openai
-import os 
 from transformers import pipeline
+import os
+
+db_password = os.getenv("DB_PASSWORD")
+
 
 app = Flask(__name__)
+
+conversational_pipeline = pipeline("conversational")
+
+# Connect to your postgres DB
+conn = psycopg2.connect(
+    dbname="database-viewpoint",
+    user="postgres",
+    password=db_password,
+    host="database-viewpoint.cbkxaouat9kh.eu-north-1.rds.amazonaws.com",
+    port="5432"
+)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+@app.route('/api/conversation', methods=['POST'])
+def conversation():
+    data = request.json
+    message = data.get('message')
+
+    # Generate a response using the conversational pipeline
+    responses = conversational_pipeline([message])
+
+    # The generated response will be the last one in the list
+    response = responses[-1]['generated_text']
+
+    # Save the conversation to the database
+    cur.execute("INSERT INTO conversations (user, message, response) VALUES (%s, %s, %s)", 
+                ('user1', message, response))
+    conn.commit()
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(port=5432)
 
 # openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -24,17 +62,3 @@ app = Flask(__name__)
 #     )
 
 #     return jsonify(response['choices'][0]['message']['content'])
-conversational_pipeline = pipeline("conversational")
-
-@app.route('/api/conversation', methods=['POST'])
-def conversation():
-    data = request.json
-    message = data.get('message')
-
-    # Generate a response using the conversational pipeline
-    responses = conversational_pipeline([message])
-
-    # The generated response will be the last one in the list
-    response = responses[-1]['generated_text']
-
-    return jsonify(response)
